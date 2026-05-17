7f87b5c0fe8846af9c8177491a5dad76c0ab6bd2
Aleksandr Bataev <aleksandr.bataev@kuper.ru>
2026-05-17 17:53:18 +0300

    Use PAT instead of GitHub OAuth

--- api/oauth/auth.js
+++ api/oauth/auth.js
@@ -1,20 +1,28 @@
-const CLIENT_ID = process.env.GH_CLIENT_ID;
+const TOKEN = process.env.GH_TOKEN;
 
 module.exports = async (req, res) => {
-    const host = req.headers['x-forwarded-host'] || req.headers.host;
-    const proto = req.headers['x-forwarded-proto'] || 'https';
-    const siteUrl = proto + '://' + host;
-    const redirectUri = siteUrl + '/api/oauth/callback';
-    const url = new URL(req.url, siteUrl);
-    const state = url.searchParams.get('state') || '';
+    const tokenJson = JSON.stringify(TOKEN);
 
-    const params = new URLSearchParams({
-        client_id: CLIENT_ID,
-        redirect_uri: redirectUri,
-        scope: 'repo',
-    });
-    if (state) params.append('state', state);
-
-    res.writeHead(302, { Location: 'https://github.com/login/oauth/authorize?' + params });
-    res.end();
+    res.setHeader('Content-Type', 'text/html;charset=utf-8');
+    res.end(
+        '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Logging in via Token...</title></head><body>' +
+        '<script>' +
+        '(function(){' +
+        'var opener=window.opener;' +
+        'if(!opener)return;' +
+        'var token=' + tokenJson + ';' +
+        'var provider="github";' +
+        'var content=JSON.stringify({token:token,provider:provider});' +
+        'function onMsg(e){' +
+        'var o=e.origin==="null"?false:e.origin;' +
+        'if(!o)return;' +
+        'window.removeEventListener("message",onMsg,false);' +
+        'opener.postMessage("authorization:"+provider+":success:"+content,o);' +
+        '}' +
+        'window.addEventListener("message",onMsg,false);' +
+        'opener.postMessage("authorizing:"+provider,"*");' +
+        '})();' +
+        '</script>' +
+        '</body></html>'
+    );
 };
